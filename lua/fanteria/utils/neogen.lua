e15a1a72ae5c76cef8209cfd4f8f60ab9aa0a7db
Jiří Alexandrovič <jiri.alexandrovic@aveco.com>
2024-06-15 21:12:09 +0200

    Change neogen placeholders text.

--- lua/fanteria/utils/neogen.lua
+++ lua/fanteria/utils/neogen.lua
@@ -3,6 +3,19 @@ local M = {}
 M.opts = {
   enabled = true,
   snippet_engine = "luasnip",
+  placeholders_text = {
+      ["description"] = "TODO: description",
+      ["tparam"] = "TODO: tparam",
+      ["parameter"] = "TODO: parameter",
+      ["return"] = "TODO: return",
+      ["class"] = "TODO: class",
+      ["throw"] = "TODO: throw",
+      ["varargs"] = "TODO: varargs",
+      ["type"] = "TODO: type",
+      ["attribute"] = "TODO: attribute",
+      ["args"] = "TODO: args",
+      ["kwargs"] = "TODO: kwargs",
+  },
 }
 
 return M
