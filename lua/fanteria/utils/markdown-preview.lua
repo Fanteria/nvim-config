e8dc501ce73e96690d44bcd919ff5d9c33090645
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2024-06-28 22:45:36 +0200

    Markdown preview custom css.

--- lua/fanteria/utils/markdown-preview.lua
+++ lua/fanteria/utils/markdown-preview.lua
@@ -4,6 +4,8 @@ M.setup = function ()
   local css_path = "css"
   local markdown_preview_css = "markdown-preview.css"
 
+  -- vim.g.mkdp_theme = "light"
+
   vim.g.mkdp_auto_close = 0
   vim.g.mkdp_page_title = "${name}"
   vim.g.mkdp_combine_preview = 1
@@ -15,7 +17,7 @@ M.setup = function ()
   local path = require("plenary.path"):new(vim.fn["stdpath"]("config"))
   path = path:joinpath(css_path):joinpath(markdown_preview_css)
   if(path:exists()) then
-    -- vim.g.mkdp_markdown_css = path.filename
+    vim.g.mkdp_markdown_css = path.filename
   else
     vim.notify("Warning: " .. path.filename .. " does not exists.", "warning")
   end
