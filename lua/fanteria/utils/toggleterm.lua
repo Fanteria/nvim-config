9e995dc4dd744f30250ecf164a97ae463fff9b2e
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2024-09-16 22:22:33 +0200

    Add terminals to open.

--- lua/fanteria/utils/toggleterm.lua
+++ lua/fanteria/utils/toggleterm.lua
@@ -1,9 +1,12 @@
 local M = {}
 
+local lua = nil
+
 M.opts = {
 
 }
 
+local fn = require("utils").fn
 M.keys = {
   {
     "<C-l>",
@@ -14,7 +17,56 @@ M.keys = {
     "<Esc>",
     "<C-\\><C-n>",
     mode = { "t" }
-  }
+  },
+  { "<leader>x", group = "Terminal" },
+  {
+    "<leader>xt",
+    fn("toggleterm.terminal", function(t)
+      if lua == nil then
+        lua = t.Terminal:new()
+        lua:toggle()
+      else
+        lua:toggle()
+      end
+    end),
+    desc = "Terminal",
+  },
+  {
+    "<leader>xl",
+    fn("toggleterm.terminal", function(t)
+      if lua == nil then
+        lua = t.Terminal:new({ cmd = "lua" })
+        lua:toggle()
+      else
+        lua:toggle()
+      end
+    end),
+    desc = "Lua",
+  },
+  {
+    "<leader>xp",
+    fn("toggleterm.terminal", function(t)
+      if lua == nil then
+        lua = t.Terminal:new({ cmd = "python" })
+        lua:toggle()
+      else
+        lua:toggle()
+      end
+    end),
+    desc = "Python",
+  },
+  {
+    "<leader>xc",
+    fn("toggleterm.terminal", function(t)
+      if lua == nil then
+        lua = t.Terminal:new({ cmd = "clang-repl" })
+        lua:toggle()
+      else
+        lua:toggle()
+      end
+    end),
+    desc = "C/C++",
+  },
 }
 
 return M
